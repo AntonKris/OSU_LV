08d2f4961a32448b23ffa9d983dfa596d31c6ba8
AntonKris <antonio.kristof7@gmail.com>
2025-03-31 23:05:42 +0200

    Add files via upload

--- LV3/zadatak_1.py
+++ LV3/zadatak_1.py
@@ -1,79 +1,72 @@
-import pandas as pd 
-import matplotlib . pyplot as plt
-
+import pandas as pd
 data = pd.read_csv("data_C02_emission.csv")
-
-
 #a)
-print("a)")
-length = len(data)
-print(f"DataFrame ima {length} mjerenja")
-
-print(f"tipovi podataka:{data.dtypes}")
+print(len(data))
+print("------------------------------------------------------------------------------------------------------")
+print(data.dtypes)
+print("------------------------------------------------------------------------------------------------------")
+data['Vehicle Class'] = data['Vehicle Class'].astype('category')
 
 print(f"Redovi s izostalim vrijednostima: {data.isnull().sum()}")
-
 print(f"Duplicirane vrijednosti: {data.duplicated().sum()}")
+print("------------------------------------------------------------------------------------------------------")
 
 #b)
-print("b)")
-largest = data.sort_values(by='Fuel Consumption City (L/100km)', ascending=False).head(3)
-print(f"Najveća gradska potrošnja:")
-print(largest[['Make', 'Model', 'Fuel Consumption City (L/100km)']])
-
-largest = data.sort_values(by='Fuel Consumption City (L/100km)', ascending=False).tail(3)
+najmanja = data.sort_values(by='Fuel Consumption City (L/100km)', ascending=False).tail(3)
 print(f"Najmanja gradska potrošnja:")
-print(largest[['Make', 'Model', 'Fuel Consumption City (L/100km)']])
+print(najmanja[['Make', 'Model', 'Fuel Consumption City (L/100km)']])
 
+najveca=data.sort_values(by="Fuel Consumption City (L/100km)",ascending=False).head(3)
+print(f"Najveća gradska potrošnja:")
+print(najveca[["Make","Model","Fuel Consumption City (L/100km)"]])
+print("------------------------------------------------------------------------------------------------------")
 
 #c)
-print("c)")
-cars = data[(data['Engine Size (L)'] > 2.5) & (data['Engine Size (L)'] < 3.5)]
-print(f"Broj automobila s motorom između 2.5 i 3.5 L: {len(cars)}")
-print(f"Prosječna CO2 emisija: {cars['CO2 Emissions (g/km)'].mean()}")
-
+inBetween=data[(data["Engine Size (L)"]>=2.5) & (data["Engine Size (L)"]<=3.5)]
+print(f"{len(inBetween)} vozila ima velicinu motora između 2.5 i 3.5")
+print(f"prosječna CO2 emisija ovih vozila je: {inBetween['CO2 Emissions (g/km)'].mean()}")
+print("------------------------------------------------------------------------------------------------------")
 #d)
-print("d)")
-audi = data[data['Make'] == 'Audi']
-print(f"Broj Audi automobila: {len(audi)}")
 
-four_cylinder_audi = audi[audi['Cylinders'] == 4]
-print(f"Prosječna CO2 emisija za Audi automobila s 4 cilindra: {four_cylinder_audi['CO2 Emissions (g/km)'].mean()}")
+audiMeasurements=data[data["Make"]=="Audi"]
+print(f"na {len(audiMeasurements)} audi vozila su se provela mjerenja")
+
+fourCyliderAudis=audiMeasurements[audiMeasurements["Cylinders"]==4]
+print(f"prosječna emisija CO2 plinova Audi vozila sa 4 cilindra je: {fourCyliderAudis['CO2 Emissions (g/km)'].mean()}")
+print("------------------------------------------------------------------------------------------------------")
 
 #e)
-print("e)")
-cylinder_amount = data['Cylinders'].unique()
-for cylinder in cylinder_amount:
-     auto_cylinders = data[data['Cylinders'] == cylinder]
-     print(f"Broj automobila s {cylinder} cilindara: {len(auto_cylinders)}")
-     print(f"Prosječna CO2 emisija: {auto_cylinders['CO2 Emissions (g/km)'].mean()}")
 
-#f
-print("f)")
-disel = data[data["Fuel Type"] == "D"]
-petrol = data[data["Fuel Type"] == "Z"]
+cylinderAmount=data["Cylinders"].unique()
+for typeofCylinder in cylinderAmount:
+    amountOfCars=data[data["Cylinders"]==typeofCylinder]
+    print(f"{typeofCylinder} cyliders:{len(amountOfCars)} cars, average CO2 emissions: {amountOfCars['CO2 Emissions (g/km)'].mean()}")
+print("------------------------------------------------------------------------------------------------------")   
 
-print(f"Prosječna gradska potrošnja dizel vozila: {disel["Fuel Consumption City (L/100km)"].mean()}")
-print(f"Medijalna vrijednost dizel vozila: {disel["Fuel Consumption City (L/100km)"].median()}")
+#f)
+dizel=data[data['Fuel Type']=="D"]
+benzin=data[data['Fuel Type']=="X"]
 
-print(f"Prosječna gradska potrošnja benzin vozila: {petrol["Fuel Consumption City (L/100km)"].mean()}")
-print(f"Medijalna vrijednost benzin vozila: {petrol["Fuel Consumption City (L/100km)"].median()}")
+print(f"prosječna gradska potrošnja dizel vozila: {dizel['Fuel Consumption City (L/100km)'].mean()}")
+print(f"prosječna gradska potrošnja vozila na regularni benzin: {benzin['Fuel Consumption City (L/100km)'].mean()}")
 
-#g
-print("g)")
+print(f"medijalna gradska potrošnja dizel vozila: {dizel['Fuel Consumption City (L/100km)'].median()}")
+print(f"medijalna gradska potrošnja vozila na regularni benzin: {benzin['Fuel Consumption City (L/100km)'].median()}")
+print("------------------------------------------------------------------------------------------------------")   
 
-four_cylinder_diesel=data[(data["Cylinders"] == 4)&(data["Fuel Type"]=="D")].max()
-print(four_cylinder_diesel)
+#g)
 
-#h
-print("h)")
+fourCylinderDiesel=data[(data["Cylinders"]==4)&(data["Fuel Type"]=="D")]
+maxFourCylinderDiesel=fourCylinderDiesel[fourCylinderDiesel['Fuel Consumption City (L/100km)']==fourCylinderDiesel['Fuel Consumption City (L/100km)'].max()]
+print("4 cilindrični dizel automobil s najvećom gradskom potrošnjom goriva je:")
+print(maxFourCylinderDiesel[['Make','Model','Fuel Consumption City (L/100km)']])
+print("------------------------------------------------------------------------------------------------------")  
+#h)
+manuals=data[data['Transmission'].str[0]=="M"]
+print(f"Broj vozila sa ručnim mjenjačom je: {(len(manuals['Make']))}")
+print("------------------------------------------------------------------------------------------------------")  
+#i)
+print(data.corr(numeric_only=True))
 
-manual_transmission= data[data['Transmission'].str.contains('AS')]
-print(f"Broj vozila s ručnim mjenjačem: {len(manual_transmission)}")
-
-#i
-print("i)")
-numeric = ['Engine Size (L)', 'Cylinders', 'Fuel Consumption City (L/100km)', 'Fuel Consumption Hwy (L/100km)', 'Fuel Consumption Comb (L/100km)', 'CO2 Emissions (g/km)']
-corr = data[numeric].corr()
-print("Korelacija numeričkih veličina:")
-print(corr)
+#Sto je negativna korelacija blize -1 to je ona vise obrnuto proporcijalna, dok sto je blize 1, to je vise proporcijonalna. Vrijednosti oko 0
+#nemaju nikakvu korelaciju s velicinom.
